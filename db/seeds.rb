3a57c12646f2572c7dc1f1340e8cc33dba705fec
cedric arnous <cedric.arnous@gmail.com>
2022-07-16 12:19:09 +0200

    articles index.html basics

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+User.destroy_all
+
+admin = User.create(email: "cedric.arnous@gmail.com", password: "123456")
+
+Article.create(title: "titre de l'article", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima eius autem voluptatem, necessitatibus numquam maxime nihil pariatur ipsam in doloribus libero totam. Ullam in dolores, porro et animi odio repudiandae?", user: admin)
+Article.create(title: "titre de l'article", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima eius autem voluptatem, necessitatibus numquam maxime nihil pariatur ipsam in doloribus libero totam. Ullam in dolores, porro et animi odio repudiandae?", user: admin)
+Article.create(title: "titre de l'article", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima eius autem voluptatem, necessitatibus numquam maxime nihil pariatur ipsam in doloribus libero totam. Ullam in dolores, porro et animi odio repudiandae?", user: admin)
+Article.create(title: "titre de l'article", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima eius autem voluptatem, necessitatibus numquam maxime nihil pariatur ipsam in doloribus libero totam. Ullam in dolores, porro et animi odio repudiandae?", user: admin)
